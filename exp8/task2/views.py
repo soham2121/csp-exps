5da25ecc09811fe5fd0e0bb96208bf47ca35d12a
Soham21 <56083422+soham2121@users.noreply.github.com>
2025-03-22 02:11:27 +0530

    Add files via upload

--- exp8/task2/views.py
+++ exp8/task2/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
-from grades import Student
+from . import grades
 
 def index(request):
     return render(request, 're.html')
 
 def calc(request):
     if request.method == "POST":
-        name = float(request.POST['Name'])
+        name = request.POST['Name']
         phy = float(request.POST['Phy'])
         chem = float(request.POST['Chem'])
         math = float(request.POST['Math'])
 
         marks = [phy, chem, math]
 
-        std = Student(name, marks)
+        std = grades.Student(name, marks)
 
         avg = std.calculate_average()
         avg_grd = std.assign_grade()
 
-        return render(request, 're.html', {'marks': avg, 'grade': avg_grd})
+        return render(request, 're.html', {'name': name, 'marks': avg, 'grade': avg_grd})
 
